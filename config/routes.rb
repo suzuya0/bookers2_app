e1f913bc94bff3aa486a9ba30ba40cda6ed6a95e
suzuya0 <sakura240613@gmail.com>
2021-08-24 03:59:48 +0000

    [Add]名前ログイン

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+  root to:'homes#top'
+  get 'about' => 'homes#about'
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+  
+  
+  resources :users,only:[:index,:show,:edit,:update]
+
+  
+  
+  
 end
